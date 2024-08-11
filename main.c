edae511fe13a277777b848422e6ee9f8af2fb329
culyun <leeson.consulting@gmail.com>
2024-08-11 21:31:44 +1200

    No-Tkt: Whitespace tabs to spaces

--- main.c
+++ main.c
@@ -3,26 +3,26 @@
 
 static THD_WORKING_AREA(waThread1, 128);
 static THD_FUNCTION(Thread1, arg) {
-	(void)arg;
+  (void)arg;
 
-	chRegSetThreadName("blinker");
-	while (true) {
-		palClearPad(GPIOC, 13U);
-		chThdSleepMilliseconds(500);
-		palSetPad(GPIOC, 13U);
-		chThdSleepMilliseconds(500);
-	}
+  chRegSetThreadName("blinker");
+  while (true) {
+    palClearPad(GPIOC, 13U);
+    chThdSleepMilliseconds(500);
+    palSetPad(GPIOC, 13U);
+    chThdSleepMilliseconds(500);
+  }
 }
 
 int main(void) {
-	halInit();
-	chSysInit();
+  halInit();
+  chSysInit();
 
-	// palSetPadMode(GPIOC, 13, PAL_MODE_ALTERNATE(1));
+  // palSetPadMode(GPIOC, 13, PAL_MODE_ALTERNATE(1));
 
-	chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO + 1, Thread1, NULL);
+  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO + 1, Thread1, NULL);
 
-	while (true) {
-		chThdSleepMilliseconds(500);
-	}
+  while (true) {
+    chThdSleepMilliseconds(500);
+  }
 }
